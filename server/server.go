0756a72d3c7f0b0274d379b713adfaedfc276278
Dias12 <diasoryntayev@gmail.com>
2022-11-02 21:37:03 +0600

    rename handlefunc

--- server/server.go
+++ server/server.go
@@ -1,14 +1,15 @@
 package server
 
 import (
+	"groupie-tracker/server/handler"
 	"log"
 	"net/http"
 )
 
 func Server() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/details", artist)
+	mux.HandleFunc("/", handler.Home)
+	mux.HandleFunc("/details", handler.Artist)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
