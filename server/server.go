f8a63e941edc85299510e573f1d02ab8184db819
Dias12 <diasoryntayev@gmail.com>
2022-11-03 17:13:16 +0600

    feat: add func GetZapros

--- server/server.go
+++ server/server.go
@@ -8,11 +8,21 @@ import (
 
 func Server() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler.Home)
-	mux.HandleFunc("/details", handler.Artist)
+	mux.HandleFunc("/", GetZapros(handler.Home))
+	mux.HandleFunc("/details", GetZapros(handler.Artist))
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func GetZapros(handleFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			handler.Err(w, http.StatusMethodNotAllowed)
+			return
+		}
+		handleFunc(w, r)
 	}
 }
