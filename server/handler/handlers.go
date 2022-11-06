96c36da0eabd8e3ac64ad853324468c94e84c2ed
Dias12 <diasoryntayev@gmail.com>
2022-11-06 16:58:02 +0600

    feat: add SearchHandler

--- server/handler/handlers.go
+++ server/handler/handlers.go
@@ -23,7 +23,6 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Artist(w http.ResponseWriter, r *http.Request) {
-
 	// detailArtist := grabjson.GetArtistById(id)
 
 	html, err := template.ParseFiles("./ui/html/details.html")
@@ -32,6 +31,23 @@ func Artist(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if err = html.Execute(w, nil); err != nil {
+		Err(w, http.StatusInternalServerError)
+		return
+	}
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("./ui/html/index.html")
+	if err != nil {
+		Err(w, http.StatusInternalServerError)
+		return
+	}
+	text := r.FormValue("search")
+	if isTextEmpty(text) {
+		return
+	}
+
+	if err = html.Execute(w, artist); err != nil { // artist it's search result
 		Err(w, http.StatusInternalServerError)
 		return
 	}
