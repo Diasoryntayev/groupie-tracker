ece05b387cdb47fd7e819208e54f67e12648785a
Dias12 <diasoryntayev@gmail.com>
2022-11-09 16:37:05 +0600

    update Artist

--- server/handler/handlers.go
+++ server/handler/handlers.go
@@ -4,6 +4,7 @@ import (
 	"groupie-tracker/server/grabjson"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -32,14 +33,33 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Artist(w http.ResponseWriter, r *http.Request) {
-	// detailArtist := grabjson.GetArtistById(id)
-
 	html, err := template.ParseFiles("./ui/html/details.html")
 	if err != nil {
 		Err(w, http.StatusInternalServerError)
 		return
 	}
-	if err = html.Execute(w, nil); err != nil {
+
+	artists, err := grabjson.GetData()
+	if err != nil {
+		Err(w, http.StatusInternalServerError)
+		return
+	}
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		Err(w, http.StatusNotFound)
+		return
+	}
+	if id < 1 || id > len(artists.Artist) {
+		Err(w, http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		Err(w, http.StatusInternalServerError)
+		return
+	}
+
+	if err = html.Execute(w, &artists.Artist[id-1]); err != nil {
 		Err(w, http.StatusInternalServerError)
 		return
 	}
