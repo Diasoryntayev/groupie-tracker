b5063e6424c44649e8fb0aba2740a76d8227f367
Dias12 <diasoryntayev@gmail.com>
2022-11-09 16:22:37 +0600

    feat: add SearchArtist

---
+++ server/grabjson/Search.go
@@ -0,0 +1,52 @@
+package grabjson
+
+import (
+	"groupie-tracker/server/model"
+	"strconv"
+	"strings"
+)
+
+func SearchArtist(searchWord string) (*model.AllArtist, error) {
+	var result []model.Artist
+	artists, err := GetData()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, artist := range artists.Artist {
+		if myContains(artist.Name, searchWord) {
+			result = append(result, artist)
+			continue
+		} else if strings.Contains(strconv.Itoa(artist.CreationDate), searchWord) {
+			result = append(result, artist)
+			continue
+		} else if strings.Contains(artist.FirstAlbum, searchWord) {
+			result = append(result, artist)
+			continue
+		} else {
+			for _, member := range artist.Members {
+				if strings.Contains(member, searchWord) {
+					result = append(result, artist)
+					break
+				}
+			}
+		}
+		for location := range artist.DatesLocations {
+			if myContains(location, searchWord) {
+				result = append(result, artist)
+				break
+			}
+		}
+		for _, dates := range artist.DatesLocations {
+			for _, date := range dates {
+				if strings.Contains(date, searchWord) {
+					result = append(result, artist)
+					break
+				}
+			}
+			break
+		}
+	}
+	allArtists := &model.AllArtist{FoundArtist: result}
+	return allArtists, nil
+}
