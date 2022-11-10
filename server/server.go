85073904b7398677b52fea9684aceb2f72d03f43
Dias12 <diasoryntayev@gmail.com>
2022-11-10 15:37:06 +0600

    handle static files

--- server/server.go
+++ server/server.go
@@ -10,7 +10,10 @@ func Server() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", GetZapros(handler.Home))
 	mux.HandleFunc("/details", GetZapros(handler.Artist))
-	mux.HandleFunc("/searched", GetZapros(handler.SearchHandler))
+	mux.HandleFunc("/searched/", GetZapros(handler.SearchHandler))
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
