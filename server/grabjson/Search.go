58f6572a9b356e5e3e8de8d2fe7c98174f8d97bf
Dias12 <diasoryntayev@gmail.com>
2022-11-22 14:03:37 +0600

    add func isEqual to avoid duplication

--- server/grabjson/Search.go
+++ server/grabjson/Search.go
@@ -15,33 +15,45 @@ func SearchArtist(searchWord string) (*model.AllArtist, error) {
 
 	for _, artist := range artists.Artist {
 		if myContains(artist.Name, searchWord) {
-			result = append(result, artist)
+			if !isEqual(artist, result) {
+				result = append(result, artist)
+			}
 			continue
 		} else if strings.Contains(strconv.Itoa(artist.CreationDate), searchWord) {
-			result = append(result, artist)
+			if !isEqual(artist, result) {
+				result = append(result, artist)
+			}
 			continue
 		} else if strings.Contains(artist.FirstAlbum, searchWord) {
-			result = append(result, artist)
+			if !isEqual(artist, result) {
+				result = append(result, artist)
+			}
 			continue
 		} else {
 			for _, member := range artist.Members {
 				if strings.Contains(member, searchWord) {
-					result = append(result, artist)
-					break
+					if !isEqual(artist, result) {
+						result = append(result, artist)
+						break
+					}
 				}
 			}
 		}
 		for location := range artist.DatesLocations {
 			if myContains(location, searchWord) {
-				result = append(result, artist)
-				break
+				if !isEqual(artist, result) {
+					result = append(result, artist)
+					break
+				}
 			}
 		}
 		for _, dates := range artist.DatesLocations {
 			for _, date := range dates {
 				if strings.Contains(date, searchWord) {
-					result = append(result, artist)
-					break
+					if !isEqual(artist, result) {
+						result = append(result, artist)
+						break
+					}
 				}
 			}
 			break
@@ -53,4 +65,13 @@ func SearchArtist(searchWord string) (*model.AllArtist, error) {
 
 func myContains(s, sub string) bool {
 	return strings.Contains(strings.ToLower(s), strings.ToLower(sub))
+}
+
+func isEqual(artist model.Artist, result []model.Artist) bool {
+	for _, v := range result {
+		if v.Name == artist.Name {
+			return true
+		}
+	}
+	return false
 }
